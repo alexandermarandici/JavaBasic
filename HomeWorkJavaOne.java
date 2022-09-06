015a87d02d18881e10d1a10d197835cb93c3578c
alexandermarandici <112718882+alexandermarandici@users.noreply.github.com>
2022-09-06 22:53:31 +0200

    Create HomeWorkJavaOne.java

---
+++ HomeWorkJavaOne.java
@@ -0,0 +1,47 @@
+/**basic_41-42e
+* Java Basic Home work 1
+*
+*@author Alexandru Marandici
+*@version 06.09.2022
+*
+*/
+class HomeWorkJavaOne {
+    public static void main(String[] args) {
+        int a = 25;
+        System.out.println(a);
+        int b = 20;
+        System.out.println(b);
+        int p = a + b * 2 ;
+        System.out.println(p);
+        
+        int c = 5;
+        System.out.println(c);
+        int d = 3;
+        System.out.println(d);
+        int e = c + d;
+        System.out.println(e);
+        int f = c * d;
+        System.out.println(f);
+        float i = c / 3f;
+        System.out.println(i);
+        
+        int[] array = new int[5];
+        array [0] = 4;
+        System.out.println(array [4]);
+        
+        int r = 27 ;
+        System.out.println(r);
+        System.out.println(Math.PI);
+        float v = 27 / 2f;
+        System.out.println(v);
+        float m = 3.14f * 13.5f * 13.5f;
+        System.out.println(m);
+        float l = 572.26f * 40f / 1f;
+        System.out.println(l);
+        
+        
+        
+        
+        
+    }
+}
