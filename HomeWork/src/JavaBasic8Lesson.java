cbebc90fb7358ffb1160dbb8b06adf5094c21200
Alexandru Marandici <112718882+alexandermarandici@users.noreply.github.com>
2022-10-02 12:36:18 +0200

    Delete HomeWork/src directory

--- HomeWork/src/JavaBasic8Lesson.java
+++
@@ -1,8 +0,0 @@
-public class JavaBasic8Lesson {
-    public static void main(String[] args) {
-        Cat cat = new Cat();
-        System.out.println("Cat: " + cat.name + ", color: " + cat.color + ", age: " + cat.age);
-
-    }
-
-}
